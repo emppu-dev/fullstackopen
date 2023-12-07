28cada0be96b0069bd2c1b978f0b641b7e7a7cc3
emppu <83163481+emppu-dev@users.noreply.github.com>
2023-12-07 20:33:52 +0200

    1.7 ✔️

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,27 +1,31 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const Button = ({ clickFunction, text }) => (
   <button onClick={clickFunction}>
     {text}
   </button>
-)
+);
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleGood = () => {
-    setGood(good + 1)
-  }
+    setGood(good + 1);
+  };
 
   const handleNeutral = () => {
-    setNeutral(neutral + 1)
-  }
+    setNeutral(neutral + 1);
+  };
 
   const handleBad = () => {
-    setBad(bad + 1)
-  }
+    setBad(bad + 1);
+  };
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+  const positivePercentage = (good / total) * 100 || 0;
 
   return (
     <div>
@@ -35,8 +39,12 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>total {total}</p>
+        <p>average {average}</p>
+        <p>positive feedback {positivePercentage}%</p>
       </div>
     </div>
-  )
-}
-export default App
+  );
+};
+
+export default App;
